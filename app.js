221099cfc494c3790688a6d2cfb266fa3c1dd963
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-05 14:55:50 +0530

    created endpoints stucture and URI for todos

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const mongoDBURL = "some url";
 const PORT = 3000;
 
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Hi I am Server", data: "no data on this endpoint" });
+});
+
+app.get("/todos", (req, res) => {
+  res.send("routes to get all the todos list");
+});
+
+app.post("/todos", (req, res) => {
+  res.send("routes to create new todo");
+});
+
+app.get("/todos/:id", (req, res) => {
+  res.send("routes to get single todo");
+});
+
+app.put("/todos/:id", (req, res) => {
+  res.send("routes to update the todo");
+});
+
+app.delete("/todos/:id", (req, res) => {
+  res.send("routes to delete the todo");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
